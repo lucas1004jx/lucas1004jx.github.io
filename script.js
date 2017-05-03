3e90dee20a13498ffb15bfa37fa40449233dbc4f
lucas1004jx <lucas1004jx@gmail.com>
2017-05-03 14:27:49 +0200

    Add files via upload
    add to the top feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//change perfil
 var myPerfil = document.getElementById("perfil");
 var imageArray = ["image/me0.jpg","image/me1.png",
                  "image/me2.png","image/me3.png","image/me4.png"];
@@ -11,3 +11,33 @@ function changePerfil(){
        }
 }
 setInterval(changePerfil, 3000);
+
+//add to the top function
+var toTop=document.getElementById("toTop");
+window.onscroll=function(){
+    
+    toTop.style.display="inline-block";
+};
+
+function disppear (){
+     var width = 0;
+    // get the width.. more cross-browser issues
+    if (window.innerHeight) {
+        width = window.innerWidth;
+    } else if (document.documentElement && document.documentElement.clientHeight) {
+        width = document.documentElement.clientWidth;
+    } else if (document.body) {
+        width = document.body.clientWidth;
+    }
+    if(width < 600){
+        
+       toTop.style.display="none";
+       }
+    
+    
+}
+
+window.onresize=function(){
+    
+    disppear();
+};
