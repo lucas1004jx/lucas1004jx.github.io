8674b06d0f19aef64657cc571080fc8b582a4c39
lucas1004jx <lucas1004jx@gmail.com>
2017-05-18 10:23:48 +0200

    Update cvScript.js

--- about/CV/cvScript.js
+++ about/CV/cvScript.js
@@ -41,13 +41,13 @@ window.onscroll=function(){
             
            
            /*position*/
-            container.style.top=e.offsetY + 50 + "px";
-            container.style.left= e.offsetX  + 50 + "px";
+            container.style.top=e.offsetY + 15 + "px";
+            container.style.left= e.offsetX  + 15 + "px";
             container.appendChild(largeImg); 
             
             e.target.addEventListener("mousemove",function(m){
-                 container.style.top =m.offsetY + 50 + "px"; 
-                container.style.left = m.offsetX +  50 +"px";
+                 container.style.top =m.offsetY + 15 + "px"; 
+                container.style.left = m.offsetX +  15 +"px";
             },false);
             
             
