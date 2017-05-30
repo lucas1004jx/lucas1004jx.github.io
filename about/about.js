4afcd0a818f6ba916805614d500a2b52f552b7cc
lucas1004jx <lucas1004jx@gmail.com>
2017-05-30 12:29:26 +0200

    improvement for about page

--- about/about.js
+++ about/about.js
@@ -1,17 +1,20 @@
 (function(){
-   var  title = document.querySelector("header h1");
-    var header = document.querySelector("header");
-    function position(){
+    
+   var  title = document.querySelector("header  h1");
+   var header = document.querySelector("header");
+    
     title.style.top=(header.offsetHeight-title.offsetHeight)/2  + "px";
     title.style.left=(header.offsetWidth-title.offsetWidth)/2 + "px";
-    }
+       
+    
     
     function rePosition(){
     title.style.top=(header.offsetHeight-title.offsetHeight)/2 + "px";
     title.style.left=(header.offsetWidth-title.offsetWidth)/2 + "px";
+       
     }
     
-    window.addEventListener("load",position,false);
+    
     window.addEventListener("resize",rePosition,false);
     
 })();
