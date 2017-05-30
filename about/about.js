ea2c05e04dd43d7b4d570aafadb9f1344a5aec8d
lucas1004jx <lucas1004jx@gmail.com>
2017-05-30 10:47:23 +0200

    add about page
    use sass ,add about page

---
+++ about/about.js
@@ -0,0 +1,17 @@
+(function(){
+   var  title = document.querySelector("header h1");
+    var header = document.querySelector("header");
+    function position(){
+    title.style.top=(header.offsetHeight-title.offsetHeight)/2  + "px";
+    title.style.left=(header.offsetWidth-title.offsetWidth)/2 + "px";
+    }
+    
+    function rePosition(){
+    title.style.top=(header.offsetHeight-title.offsetHeight)/2 + "px";
+    title.style.left=(header.offsetWidth-title.offsetWidth)/2 + "px";
+    }
+    
+    position();
+    window.addEventListener("resize",rePosition,false);
+    
+})();
