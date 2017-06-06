6f529d7b1395a1bc396e180129a96e50472b9468
lucas1004jx <lucas1004jx@gmail.com>
2017-06-06 23:29:16 +0200

    add new home page

--- about/about.js
+++ about/about.js
@@ -1,18 +1,39 @@
-(function(){
-    
-   var  title = document.querySelector("header  h1");
-   var header = document.querySelector("header");
-    
-    
-    title.style.top=(header.offsetHeight-title.offsetHeight)/2  + "px";
-    title.style.left=(header.offsetWidth-title.offsetWidth)/2 + "px";
-    
-    function rePosition(){
-    title.style.top=(header.offsetHeight-title.offsetHeight)/2 + "px";
-    title.style.left=(header.offsetWidth-title.offsetWidth)/2 + "px";
-       
+(function () {
+
+    var title = document.querySelector("header  h1");
+    var header = document.querySelector("header");
+
+
+    title.style.top = (header.offsetHeight - title.offsetHeight) / 2 + "px";
+    title.style.left = (header.offsetWidth - title.offsetWidth) / 2 + "px";
+
+
+    function rePosition() {
+        title.style.top = (header.offsetHeight - title.offsetHeight) / 2 + "px";
+        title.style.left = (header.offsetWidth - title.offsetWidth) / 2 + "px";
+
     }
-    
-    window.addEventListener("resize",rePosition,false);
-    
+
+    window.addEventListener("resize", rePosition, false);
+
 })();
+
+//to top function
+$(document).ready(function () {
+
+    $("#toTop button").on("click", function () {
+        $("html body").animate({scrollTop:0},"slow");
+    });
+
+
+
+$(window).on("scroll",function(){
+    if ($(window).scrollTop() > 250) {
+        
+        $("#toTop button").css("display", "block");
+    }else{
+        $("#toTop button").css("display", "none");
+    }
+});
+    
+});
--- about/travel/travel.js
+++ about/travel/travel.js
@@ -1,25 +1,32 @@
-$(".carousel").carousel({
-    interval:3000,
-    pause:"hover"
-});
-/*to top function*/
-(function(){
-    var btn = document.querySelector("#toTop button");
-    function top(){
-        document.body.scrollTop=0;
-        document.documentElement.scrollTop=0;
-    }
-    btn.addEventListener("click",top,false);
-    
-    window.addEventListener("scroll",function(){
-        
-        if(document.body.scrollTop > 250 || document.documentElement.scrollTop > 250){
-            btn.style.display ="block";
-           }else{
-               
-               btn.style.display="none";
-           }
-        
+
+
+$(function () {
+    $(".carousel").carousel({
+        interval: 3000,
+        pause: "hover"
     });
-    
-})()
+})
+
+/*to top function*/
+$(function () {
+    var btn = document.querySelector("#toTop button");
+
+    function top() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+    btn.addEventListener("click", top, false);
+
+    window.addEventListener("scroll", function () {
+
+        if (document.body.scrollTop > 250 ||  document.documentElement.scrollTop > 250) {
+            btn.style.display = "block";
+        } else {
+
+            btn.style.display = "none";
+        }
+
+    });
+
+})
+
