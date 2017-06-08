24fbead8cd2b8866d8bf0a6d330fc98a8dabf295
lucas1004jx <lucas1004jx@gmail.com>
2017-06-09 00:07:43 +0200

    add fade in and out animation to home page element

--- script.js
+++ script.js
@@ -43,6 +43,26 @@ $(function(){
            }
     })
     
+    /*animate visible element on screen*/
+    
+    $(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade, #timeLine h4,#timeLine p,#timeLine i").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(1000,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(1000,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
+
+    
 });
 
 
