e4c224664d9581118195a7a657f1e7da77acc70c
lucas1004jx <lucas1004jx@gmail.com>
2017-06-19 13:15:23 +0200

    add local weather page

--- project/local weather/node_modules/dark-sky-api/node_modules/geo-loc-utils/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./dist/geo-loc-utils.js');
--- project/local weather/script/weather.js
+++ project/local weather/script/weather.js
@@ -3,13 +3,18 @@
 //google geo api AIzaSyAnIcTjsNB5mCVOHW7KUuJVIR9kFV1G28E
 //var url= "https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY";
 
+//get icon http://openweathermap.org/img/w/10d.png
+
+//------------weather api-------------------
 $(function(){
     var lat=0;
     var lng=0;
     var url="";
     var api="0b723ce4aade68f38bd6fb0ae3303df4";
-   
+   var degC;
+    var degF;
     
+    getLocation();
     
     function getInfo(){
         
@@ -22,14 +27,22 @@ $(function(){
     
     function weatherInfo(response){
         var condition = response.weather[0].main;
-        var maxDegC = Math.floor(response.main.temp_max-273.15);
-        var minDegC = Math.floor(response.main.temp_min-273.15);
-        var maxDegF = maxDegC*1.8+32;
-        var minDegF = minDegC*1.8+32;
+        var descri=response.weather[0].description;
+        degC = Math.floor(response.main.temp_max-273.15);
+        degF = Math.floor(degC*1.8+32);
+        var windSpeed=response.wind.speed;
         var humidity = response.main.humidity + "%";
         var city=response.name;
-        var info ="You are in "+city+"<br>Current weather condition is:"+condition+"<br>Max temperature is: "+ maxDegC + "&#176;C/" + maxDegF +"&#176;F"+"<br>Min temperature is: "+ minDegC + "&#176;C/" + minDegF +"&#176;F" +"<br>The humidity is: "+ humidity;
-        $("#weather").html("<p>" + info + "</p>");
+        var country=response.sys.country;
+        var icon=response.weather[0].icon;
+        /*var info ="You are in "+city+", "+country+ "<br>Current weather condition: "+condition+"<br>Current temperature: "+ degC + "&#176;C/" + degF +"&#176;F"+"<br>Current humidity : "+ humidity+"<br>Current wind speed: "+ windSpeed + "mps";
+        
+        $("#weather").append("<p>" + info + "</p>");*/
+        $("#city").html("<h1>"+city+"</h1>");
+        $("#descri").html("<p>"+descri+"</p>");
+         $("#temp").html("<h1>"+degC+"&#176;C</h1>");
+        $("#weather img").attr("src","http://openweathermap.org/img/w/" + icon +".png");
+        
     }
     
     function getLocation(){
@@ -41,15 +54,69 @@ $(function(){
                
             url="http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng;
              console.log(url);  
+               
+               getInfo();
+               
            },function(){
-              $("#weather").html("<p>can´t get your location</p>");
+              $("#weather").html("<p>Sorry, can´t get your location, please refresh the browser agian</p>");
            });
-           }
-       
+            
+           }/*----if navagator*/
+        
+        
     }
-});
-  
 
+  
+//-----------------main js-------------------------
+
+    
+    //-----------fullheight function
+   function fullHeight(){
+       var wHeight=$(window).height();
+    $(".fullHeight").css("height",wHeight);
+   }
+    
+    fullHeight();
+    $(window).resize(fullHeight);
+    
+    /*---------------turn to Fahrenheit btn*/
+    $("#deg").click(function(){
+        var control=$("#deg").attr("data-deg");
+        
+        if(control==="c"){
+           $("#temp").html("<h1>"+degF+"&#176;F</h1>");
+        $("#deg").text("Turn to Celsius").attr("data-deg","f");
+    
+           } else if(control==="f"){
+        $("#temp").html("<h1>"+degC+"&#176;C</h1>");
+        $("#deg").text("Turn to Fahrenheit").attr("data-deg","c");
+                     }
+        
+        
+    });
+    
+    
+    /*-------------------navbar overlay------------*/
+            $("#open").click(function() {
+                    $("#nav").fadeToggle();
+                $("#open").css("visibility","hidden");
+               
+                });
+            $("#close").click(function(){
+                $("#nav").fadeToggle();
+               $("#open").css("visibility","visible");
+            });              
+            
+    $(window).resize(function() {
+        if (window.innerWidth >=769) {
+            $("#nav").css("display", "block");
+        }else{
+            $("#nav").css("display", "none");
+            $("#open").css("visibility","visible");
+        }
+
+    });      
+});
   
 
 
